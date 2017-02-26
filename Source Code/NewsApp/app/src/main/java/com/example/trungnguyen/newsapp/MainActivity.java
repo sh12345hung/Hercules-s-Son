62c92920dedd1d39f809f312ace9ad377553e378
Trung Nguyen <trungnguyenduy139@gmail.com>
2017-02-27 01:49:38 +0700

    Add: commit android source

---
+++ Source Code/NewsApp/app/src/main/java/com/example/trungnguyen/newsapp/MainActivity.java
@@ -0,0 +1,126 @@
+package com.example.trungnguyen.newsapp;
+
+import android.content.DialogInterface;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.example.trungnguyen.newsapp.adapter.ViewPagerAdapter;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    Toolbar toolbar;
+    TabLayout tab;
+    ViewPager viewPager;
+    ActionBarDrawerToggle drawerToggle;
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        addControls();
+        setTitle(getResources().getString(R.string.app_name));
+        setSupportActionBar(toolbar); // vì ta đang sử dụng gói AppCompatActivity tức gói thư viện hổ trợ
+        ViewPagerAdapter pagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(pagerAdapter);
+        tab.setupWithViewPager(viewPager);
+        drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close); //drawerToggle phải đc khỏi tạo sau toolbar
+        drawerLayout.addDrawerListener(drawerToggle);
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        drawerToggle.syncState();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_homepage, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (drawerToggle.onOptionsItemSelected(item))
+            return true;
+        int itemId = item.getItemId();
+        switch (itemId) {
+            case R.id.menuAbout:
+        }
+        return true;
+    }
+
+    private void addControls() {
+        toolbar = (Toolbar) findViewById(R.id.toolbarID);
+        tab = (TabLayout) findViewById(R.id.tab);
+        viewPager = (ViewPager) findViewById(R.id.viewPager);
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+        navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(MainActivity.this);
+    }
+
+    private void changeViewPagerPage(final int position) {
+        viewPager.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                viewPager.setCurrentItem(position, true);
+            }
+        }, 100);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.drawMenuTheGioi)
+            changeViewPagerPage(0);
+        else if (id == R.id.drawMenuTheThao)
+            changeViewPagerPage(1);
+        else if (id == R.id.drawMenuCongNghe)
+            changeViewPagerPage(2);
+        else if (id == R.id.drawMenuGiaiTri)
+            changeViewPagerPage(3);
+        else if (id == R.id.drawMenuThoiTrang)
+            changeViewPagerPage(4);
+        else if (id == R.id.drawMenuSucKhoe)
+            changeViewPagerPage(5);
+        else
+            changeViewPagerPage(6);
+
+        Log.d("KIEMTRA", "onNavigationItemSelected " + viewPager.getCurrentItem());
+
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START))
+            drawerLayout.closeDrawer(GravityCompat.START);
+        else {
+            AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+            dialog.setTitle("Thoát ứng dụng");
+            dialog.setMessage("Bạn có muốn thoát ứng dụng?");
+            dialog.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    finish();
+                }
+            });
+            dialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    dialogInterface.dismiss();
+                }
+            });
+            dialog.show();
+        }
+    }
+}
