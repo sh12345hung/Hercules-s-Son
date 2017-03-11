7df9efffdde26fe2c658651b67f00e4c559429da
Trung Nguyen <trungnguyenduy139@gmail.com>
2017-03-11 23:06:30 +0700

    Add: Login News App with facebook API for developers

--- Source Code/NewsApp/app/src/main/java/com/example/trungnguyen/newsapp/MainActivity.java
+++ Source Code/NewsApp/app/src/main/java/com/example/trungnguyen/newsapp/MainActivity.java
@@ -19,9 +19,19 @@ import android.view.View;
 import android.widget.Button;
 
 import com.example.trungnguyen.newsapp.adapter.ViewPagerAdapter;
+import com.facebook.AccessToken;
+import com.facebook.AccessTokenTracker;
+import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.login.LoginManager;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener,
         View.OnClickListener {
+    private static final String TAG = MainActivity.class.getSimpleName();
     Toolbar toolbar;
     TabLayout tab;
     Button btSearch;
@@ -29,13 +39,48 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     ActionBarDrawerToggle drawerToggle;
     DrawerLayout drawerLayout;
     NavigationView navigationView;
+    AccessToken mAccessToken;
+    String facebookUserName;
+    boolean isUserLogin = false;
+    String loginMenuTitle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "oncreate");
+        FacebookSdk.sdkInitialize(this.getApplicationContext());
         setContentView(R.layout.activity_main);
+        isUserLogin = getIntent().getBooleanExtra(WelcomeActivity.IS_LOGIN, false);
+        if (isUserLogin) {
+            AccessTokenTracker tracker = new AccessTokenTracker() {
+                @Override
+                protected void onCurrentAccessTokenChanged(AccessToken oldAccessToken, AccessToken currentAccessToken) {
+                    mAccessToken = currentAccessToken; // get new access token the first time login
+                }
+            };
+
+            mAccessToken = AccessToken.getCurrentAccessToken(); // need use this line, if AccessToken is saved on your app
+
+            GraphRequest request = GraphRequest.newMeRequest(mAccessToken, new GraphRequest.GraphJSONObjectCallback() {
+                @Override
+                public void onCompleted(JSONObject object, GraphResponse response) {
+                    Log.d(TAG, "complete Graph API");
+                    try {
+                        facebookUserName = object.getString("name");
+                        invalidateOptionsMenu();
+                        Log.d(TAG, facebookUserName);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+            Bundle parameters = new Bundle();
+            parameters.putString("fields", "id,name,link, email");
+            request.setParameters(parameters);
+            request.executeAsync();
+        }
         addControls();
-        setSupportActionBar(toolbar); // vì ta đang sử dụng gói AppCompatActivity tức gói thư viện hổ trợ
+        setSupportActionBar(toolbar); // Because we are using AppCompat, that is support library
         ViewPagerAdapter pagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
         viewPager.setAdapter(pagerAdapter);
         tab.setupWithViewPager(viewPager);
@@ -58,7 +103,22 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
             return true;
         int itemId = item.getItemId();
         switch (itemId) {
+            case R.id.menuLogin:
+                if (item.getTitle() == loginMenuTitle) {
+                    Intent intent = new Intent(MainActivity.this, WelcomeActivity.class);
+                    startActivity(intent);
+                    finish();
+                } else {
+                    //todo: move to profile activity
+                }
+                break;
             case R.id.menuAbout:
+                break;
+            case R.id.menuLogout:
+                LoginManager.getInstance().logOut();
+                isUserLogin = false;
+                invalidateOptionsMenu();
+                break;
         }
         return true;
     }
@@ -72,6 +132,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         navigationView.setNavigationItemSelectedListener(MainActivity.this);
         btSearch = (Button) findViewById(R.id.btSearch);
         btSearch.setOnClickListener(this);
+        loginMenuTitle = getString(R.string.log_in);
     }
 
     private void changeViewPagerPage(final int position) {
@@ -115,11 +176,11 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
             dialog.setTitle("Thoát ứng dụng")
                     .setMessage("Bạn có muốn thoát ứng dụng?")
                     .setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialogInterface, int i) {
-                    finish();
-                }
-            });
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            finish();
+                        }
+                    });
             dialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
                 @Override
                 public void onClick(DialogInterface dialogInterface, int i) {
@@ -134,5 +195,22 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     public void onClick(View view) {
         Intent searchIntent = new Intent(MainActivity.this, SearchingActivity.class);
         startActivity(searchIntent);
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        MenuItem item;
+        if (isUserLogin) {
+            item = menu.findItem(R.id.menuLogin);
+            item.setTitle(facebookUserName);
+            item = menu.findItem(R.id.menuLogout);
+            item.setVisible(true);
+        } else {
+            item = menu.findItem(R.id.menuLogout);
+            item.setVisible(false);
+            item = menu.findItem(R.id.menuLogin);
+            item.setTitle(loginMenuTitle);
+        }
+        return super.onPrepareOptionsMenu(menu);
     }
 }
--- Source Code/NewsApp/app/src/main/java/com/example/trungnguyen/newsapp/WelcomeActivity.java
+++ Source Code/NewsApp/app/src/main/java/com/example/trungnguyen/newsapp/WelcomeActivity.java
@@ -3,44 +3,122 @@ package com.example.trungnguyen.newsapp;
 import android.annotation.TargetApi;
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.os.Build;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
 import android.view.View;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+
 public class WelcomeActivity extends AppCompatActivity implements View.OnClickListener {
+    private static final String TAG = WelcomeActivity.class.getSimpleName();
+    public static final String IS_LOGIN = "is_login";
     TextView tvSkip;
     ProgressBar progressBar;
+
+    CallbackManager callbackManager;
 
     @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        FacebookSdk.sdkInitialize(this.getApplicationContext());
+
         setContentView(R.layout.activity_welcome);
+
         tvSkip = (TextView) findViewById(R.id.tvSkip);
 
         progressBar = (ProgressBar) findViewById(R.id.progressBar);
 
+        callbackManager = CallbackManager.Factory.create();
+
+        AccessToken accessToken = AccessToken.getCurrentAccessToken();
+        if (accessToken != null) {
+            moveToMainActivity(true);
+        }
+        LoginButton loginButton = (LoginButton) findViewById(R.id.login_button);
+
+        loginButton.setReadPermissions(Arrays.asList("public_profile", "email"));
+
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                Log.d("TEST", "THANH CONG");
+                moveToMainActivity(true);
+            }
+
+            @Override
+            public void onCancel() {
+                Log.d("TEST", "TAT DANG NHAP");
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                Log.d("TEST", "LOI ROI: ");
+            }
+        });
 
         tvSkip.setOnClickListener(this);
+
+//        try {
+//            PackageInfo info = getPackageManager().getPackageInfo(
+//                    "com.example.trungnguyen.newsapp",
+//                    PackageManager.GET_SIGNATURES);
+//            for (Signature signature : info.signatures) {
+//                MessageDigest md = MessageDigest.getInstance("SHA");
+//                md.update(signature.toByteArray());
+//                Log.d("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+//            }
+//        } catch (PackageManager.NameNotFoundException e) {
+//
+//        } catch (NoSuchAlgorithmException e) {
+//
+//        }
 
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent mainIntent = new Intent(WelcomeActivity.this, MainActivity.class);
-                startActivity(mainIntent);
-                finish();
             }
         }, 3000);
     }
 
-    @Override
-    public void onClick(View view) {
+    private void moveToMainActivity(boolean isLogin) {
         Intent mainIntent = new Intent(WelcomeActivity.this, MainActivity.class);
+        mainIntent.putExtra(IS_LOGIN, isLogin);
         startActivity(mainIntent);
         finish();
     }
+
+    @Override
+    public void onClick(View view) {
+        moveToMainActivity(false);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        // Must set callbackManager.onActivityResult(requestCode, resultCode, data);
+        // to create facebook login button callback to get results of your login
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
 }
