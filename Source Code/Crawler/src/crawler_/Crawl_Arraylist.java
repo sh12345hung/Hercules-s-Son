bd4cd983f85bfb0c38b453c9c4117548f5f42236
Puck <trnavinhphuc111996@gmail.com>
2017-04-02 01:50:09 +0700

    Add: Chuyen thanh nhieu class extends

---
+++ Source Code/Crawler/src/crawler_/Crawl_Arraylist.java
@@ -0,0 +1,15 @@
+package crawler_;
+
+import java.util.ArrayList;
+	
+public abstract class Crawl_Arraylist {
+	    
+	public abstract ArrayList<Data> Process_Web();
+	/*
+	 *  Sử dụng abstract để ta chỉ cần định nghĩa lại hàm Process_web 
+	 *  khi mỗi lần sử dụng.
+	 *  
+	 */
+
+}
+
