5bbec8c391e555f68295b52ac21764b5ecc237ed
Puck <trnavinhphuc111996@gmail.com>
2017-04-09 11:07:24 +0700

    Add: ZingNews , Fixing Title

--- Source Code/Crawler/_NewsCrawler(New)/src/com/crawl/Main.java
+++ Source Code/Crawler/_NewsCrawler(New)/src/com/crawl/Main.java
@@ -2,24 +2,36 @@ package com.crawl;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import com.mongodb.BasicDBObject;
 
 public class Main {
 	   public static void main(String[] args) throws IOException {
-	      VnExpress();
-	      
+	     //VnExpress();
+	      ZingNews();
 	}
 
 public static void VnExpress(){
+	String web = "http://vnexpress.net/";
 	ArrayList<Data> list = new ArrayList<Data>();
-	VnExpress a = new VnExpress();
+	VnExpress a = new VnExpress(web);
+	list = a.Process_Web();
+	for(int i = 0 ; i< list.size(); i++)
+	{
+		
+	}
+	
+	
+}
+public static void ZingNews(){
+	String web = "http://news.zing.vn/";
+	ArrayList<Data> list = new ArrayList<Data>();
+	ZingNews a = new ZingNews(web);
 	list = a.Process_Web();
 	for(int i = 0 ; i< list.size(); i++)
 	{
 		/*
-		 * 
+		 * Lưu vào DataBase
 		 */
 	}
-	
-	
 }
 }
